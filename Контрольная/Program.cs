f6285ba7aece4ba05224709d0424c97fd828c22d
ZadRotaS <Ops0728@gmail.com>
2022-09-18 15:40:46 +0500

    Сделал выбор между вариантами решения

--- Контрольная/Program.cs
+++ Контрольная/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] text = { "Решение 1: простой вывод в консоль.", "Решение 2: запись в другой массив" };
+Console.Write("Выберите решение: {0} :", string.Join(" ", text));
+int x = Convert.ToInt32(Console.ReadLine());
+if (x > 2) Console.WriteLine("ERROR Вы не выбрали задачу.");
+if (x < 1) Console.WriteLine("ERROR Вы не выбрали задачу.");
